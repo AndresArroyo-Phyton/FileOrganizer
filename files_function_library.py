f07c337d4d55f0ffb4f49b10905ac34ce12cff72
Andres Arroyo Chaves <118855479+AndresArroyo1980@users.noreply.github.com>
2026-03-31 12:53:24 -0700

    Add files via upload
    First version of phyton function file with the logic and function  declaration to organize files on folders by creation year and month

---
+++ files_function_library.py
@@ -0,0 +1,195 @@
+# Import standard modules
+import shutil
+import os
+import filecmp
+
+# Load libraries
+from pathlib import Path
+from datetime import datetime
+
+
+def check_folder (user_dest_path):
+    # user_dest_path is the destination path selected by the user
+    folder_path = Path(user_dest_path)                     
+    # Set returning control variables to default value
+    exist = False
+    # Check if the user selected folder path exists
+    if folder_path.is_dir():
+        exist = True
+    # Return control variable
+    return exist
+
+
+def check_create_folder (user_dest_path):
+    # user_dest_path is the destination path selected by the user
+    folder_path = Path(user_dest_path)                     
+    # Set returning control variables to default value
+    exist = False
+    created = False
+    # Check if the user selected folder path exists
+    if folder_path.is_dir():
+        exist = True
+    # Else then create the folder
+    else:                                                  
+        Path(user_dest_path).mkdir(parents=True, exist_ok=True)
+        created = True
+    # Return control variables
+    return exist, created                                   
+
+
+def check_list_files (user_orig_path):
+    # user_orig_path is the origin path selected by the user
+    files = [f.name for f in Path(user_orig_path).iterdir() if f.is_file()]
+    # Return file list
+    return files
+
+
+def get_date_file (file_path):
+    # Get stats object
+    file = Path(file_path)
+    stats = file.stat()
+    # Convert timestamp to readable
+    mod_time = datetime.fromtimestamp(stats.st_mtime)
+    # Get year and month from the information if the file creation date
+    year = mod_time.year
+    month = mod_time.month
+    # Give format to the information to return
+    formatedyear = f"{year:04d}"
+    formatedmonth = f"{month:02d}"
+    return  formatedyear, formatedmonth
+
+
+def get_year_month ():
+    curr_date = datetime.now() 
+    # get the year
+    year = curr_date.year
+    # get the month (as an integer 1-12)
+    month = curr_date.month
+    # get the day (as an integer 1-12)
+    day = curr_date.day
+    # format the time as a string "HH:MM:SS" (24-hour format)
+    current_time_string = curr_date.strftime("%H:%M:%S")
+    # format year and month
+    formated_year = f"{year:04d}"
+    formated_month = f"{month:02d}"
+    formated_day = f"{day:02d}"    
+    return current_time_string, formated_year, formated_month, formated_day 
+
+
+def write_log_file (new_text_line):
+    # get current time
+    current_time, year, month, day = get_year_month()
+    # get current directory
+    current_directory = os.getcwd()
+    # creates the path
+    file_path =  current_directory + "\\" + year + "_" + month + "_" + 'file_organizer.txt'
+    # creates the content to write
+    content = (year + "-" + month + "-" + day +" " + current_time + "> " + new_text_line)
+
+    # open the file in append mode ('a')
+    with open(file_path, 'a') as f:     
+        # write the new line of content
+        # the '\n' ensures the text is on its own line
+        f.write(content + '\n')
+    return
+
+
+def copy_files_directory_error (source, destination):
+    try:
+        # shutil.copy2 copies the file and its metadata
+        error = False
+        shutil.copy2(source, destination)
+        text = ("File copied successfully source: " + str(source) + " to: " + str(destination))
+        write_log_file(text)
+    except FileNotFoundError:
+        error = True
+        write_log_file("Error: The source file was not found.")
+    except PermissionError:
+        error = True
+        write_log_file("Error: Permission denied at the source or destination.")
+    except shutil.SameFileError:
+        error = True
+        write_log_file("Error: Source and destination are the same file.")
+    except Exception as e:
+        error = True
+        write_log_file(f"An unexpected error occurred: {e}")
+    return error
+
+
+def get_match_files(source_file, destination_file):
+    match = filecmp.cmp(source_file, destination_file, shallow=False)
+    write_log_file("Files match" if match else "Files differ")
+    return match
+
+
+def erase_file (file_orig_path):
+    file_path = Path(file_orig_path)
+    deleted = False
+    # Attempt to delete the file
+    try:
+        file_path.unlink()
+        write_log_file(f"File '{file_path}' has been deleted.")
+        deleted = True
+    except FileNotFoundError:
+        write_log_file(f"Error: The file '{file_path}' does not exist.")
+    return deleted
+
+
+
+
+
+
+
+
+
+
+
+#---------------------------------Proposed Logic-----------------------
+#example path syntaxis "C:\\01 Andres\\google-python-exercises\\2026"
+#please type the destination file path in next variable declaration
+user_dest_path = "C:\\01 Andres\\google-python-exercises"
+#please type the file origin path in next variable declaration
+user_orig_path = "C:\\01 Andres\\google-python-exercises\\2026"
+#if you want to keep original files in next variable declaration
+#please type True if you want to erase original files or type False
+pref_erase_origin = False
+
+
+
+# Check selected folder
+exist = check_folder(user_dest_path)
+text = ("Original Folder: " + str(user_dest_path) + " Exist: " + str(exist))
+write_log_file (text)
+files = check_list_files(user_orig_path)
+text =  ("Files found: " + str(files))
+write_log_file (text)
+
+for item in files:
+    
+    file_orig_path = user_orig_path + "\\" + item
+    write_log_file (file_orig_path)
+    year, month = get_date_file (file_orig_path)
+    text = ("Creation year: " + str(year) + " month: " + str(month))
+    write_log_file (text)
+
+    final_dest_path = user_dest_path + "\\" + year + "\\" + month
+    text = ("Destiny Folder: " + str(final_dest_path))
+    write_log_file (text)
+    exist, created = check_create_folder(final_dest_path)
+    text = ("Folder exist: " + str(exist) + " created:" + str(created))
+    write_log_file(text)
+    
+    error = copy_files_directory_error(file_orig_path , final_dest_path)
+    text = ("Copy error: " + str(error))
+    write_log_file(text)
+    
+
+    copied_file_path = final_dest_path + "\\" + item
+    match = get_match_files(file_orig_path, copied_file_path)
+
+    # Check copy error and file match conditions and general preference erase origin is true
+    if ((match) and (not error) and pref_erase_origin):
+        text = ("Deleting file: " + str(file_orig_path))
+        write_log_file(text)
+        erase_file(file_orig_path)
+        
